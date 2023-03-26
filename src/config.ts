23c2fa80887a14cb6fdc2c38a7ebb6ac525362b0
voyager-gold <voyagergoldev@gmail.com>
2023-03-25 18:35:54 -0600

    load contract addresses from .env

--- src/config.ts
+++ src/config.ts
@@ -4,17 +4,17 @@ import NameWrapper from '~/abis/NameWrapper.json'
 import { ContractConfig } from './types'
 
 export const nameWrapperContract: ContractConfig = {
-  address: "0x69e756D56762Fc66ADE2eA8Da4cEd4F888d0cf8A",
+  address: import.meta.env.VITE_NAME_WRAPPER_ADDRESS,
   abi: NameWrapper as Abi
 }
 
 export const baseRegistrarContract: ContractConfig = {
-  address: "0x91cA002f8217b939a4f24EeCB992e07dCDecA32c",
+  address: import.meta.env.VITE_BASE_REGISTRAR_ADDRESS,
   abi: erc721ABI as Abi
 }
 
 export const resolverContract: ContractConfig = {
-  address: '0xb3033Ec63c6fCc3A24a3D74C1E9d88CeaBE9c4c6',
+  address: import.meta.env.VITE_RESOLVER_ADDRESS,
   abi: [] as Abi
 }
 
